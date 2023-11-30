05bd939c01b83f507970100db49f05df9066d600
test commit <idokrupik@gmail.com>
2023-11-30 16:37:15 +0200

    with jwt authentication

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flask_bcrypt import Bcrypt
+flask_bcrypt = Bcrypt()
 
 db = SQLAlchemy()
 
@@ -9,4 +11,5 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    flask_bcrypt.init_app(app)
     return app
